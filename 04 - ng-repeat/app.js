9c4fcebf9781bdceac55453710832b803b528e0f
TaiPham.IT <taipham.it@gmail.com>
2014-09-08 14:14:20 +0700

    update source

---
+++ 04 - ng-repeat/app.js
@@ -0,0 +1,30 @@
+angular.module("myApp", [])
+    .controller("myCtrl", ['$scope', function ($scope) {
+        $scope.data = [
+            {
+                "firstName": "John",
+                "lastName": "Kenny",
+                "Phone": "+84 184 181"
+            },
+            {
+                "firstName": "Pham",
+                "lastName": "Tai",
+                "Phone": "+43 310 210"
+            },
+            {
+                "firstName": "Tran",
+                "lastName": "Khanh",
+                "Phone": "+84 234 234"
+            },
+            {
+                "firstName": "Cong",
+                "lastName": "Vinh",
+                "Phone": "+12 222 121"
+            },
+            {
+                "firstName": "Nguyen",
+                "lastName": "Long",
+                "Phone": "+52 210 001"
+            }
+        ];
+    }]);
