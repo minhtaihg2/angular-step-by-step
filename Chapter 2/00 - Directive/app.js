0a3192c1c8443d4a937db48346daa1c49e346e58
Taipham-it <taipham.it@gmail.com>
2014-09-10 16:20:10 +0700

    update directive

--- Chapter 2/00 - Directive/app.js
+++ Chapter 2/00 - Directive/app.js
@@ -12,7 +12,7 @@ app.directive("myDirectiveOne", function () {
     return {
         restrict: 'E', // khai báo directive có dạng Element (C : class,A : attributes,M : comment)
         template: '<p>Hello world</p>', // render template
-        replace : true // loại bỏ thẻ directive đi
+        replace: true // loại bỏ thẻ directive đi
     }
 });
 
@@ -36,13 +36,12 @@ app.directive("myDirectiveTwoScope", function () {
         controller: function ($scope) {  // tạo 1 controller xử lý dữ liệu
             $scope.greet = "world"
         }
-      /*  ,
-        scope : {}*/
+        /*  ,
+         scope : {}*/
 
-         // cô lập phạm vi hoạt động của từng scope trong directive
+        // cô lập phạm vi hoạt động của từng scope trong directive
     }
 });
-
 
 
 // directive 4
@@ -51,16 +50,49 @@ app.directive("myDirectiveThree", function () {
     return {
         restrict: 'E',
         template: ' <input type="text" ng-model="name" placeholder="enter name"/> <p>Hello {{name}}</p>',
-        scope : {
-            user : '@someAttrs',
-            company : '@'
+        scope: {
+            user: '@someAttrs',
+            company: '@'
         },
-        controller : function($scope){
+        controller: function ($scope) {
             $scope.name = $scope.user;
         },
-        link : function(scope,ele,attrs){
+        link: function (scope, ele, attrs) {
             console.log(scope.user);
             console.log(scope.company);
+        }
+    }
+});
+app.directive("paper", function () {
+    return {
+        restrict: 'EA',
+        transclude : true,
+        template: '<div ng-hide="isDisabled">' +
+            '<button class="btn btn-primary" ng-click="disable()">Click me</button>' +
+            '<p>Directive {{action}}</p>' +
+            '</div>' +
+            '<p ng-show="isDisabled">$timeout : {{count}}</p>' +
+            '<p ng-transclude></p>',
+        controller: function ($scope, $timeout, $log) {
+            var myTime;
+            $scope.action = "Controller";
+            $scope.isDisabled = false;
+            $scope.disable = function () {
+                $scope.isDisabled = true;
+                $scope.count = 1;
+                myTime = setInterval(function () {
+                    $scope.$apply(function(){
+                        $scope.count += 1;
+                    });
+                    if ($scope.count == 6) {
+                        clearInterval(myTime);
+                    }
+                }, 1000);
+                $timeout(function () {
+                    $scope.isDisabled = false;
+                }, 5000);
+            };
+
         }
     }
 });
