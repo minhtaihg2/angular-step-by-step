9a983056dd81267a9cd897dc872e514fc29e3901
taipham <taipham.it@gmail.com>
2014-10-02 11:29:44 +0700

    update server node

--- Chapter 2/01 - $http basics/app.js
+++ Chapter 2/01 - $http basics/app.js
@@ -2,10 +2,10 @@ angular.module("myApp", [])
     .controller("myCtrl", ['$scope', '$http', function ($scope, $http) {
         $http({
             method : 'GET',
-            url : 'http://localhost:3000/posts/54291e70048d957e023a305d'
+            url : 'data.json'
         }).success(function (data, status, headers, config) {
-           // $scope.data = data;
-            console.log('data :',data);
+            $scope.data = data;
+           // console.log('data :',data);
             console.log(status); // status headers
             console.log(config); // show object
         }, function (data, status, headers, config) {
